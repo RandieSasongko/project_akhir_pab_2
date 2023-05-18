fe86545b1fc38291755a8feed2117ac1127fb486
RandieSasongko <113977205+RandieSasongko@users.noreply.github.com>
2023-05-18 15:00:02 +0700

    5/18/2023 2.59 PM

---
+++ app/src/main/java/com/example/project_akhir_uas_pemogramman_aplikasi_bergerak_ii/UbahActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_akhir_uas_pemogramman_aplikasi_bergerak_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
