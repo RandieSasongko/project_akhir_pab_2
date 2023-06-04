f7cc02365c2336f65e5c61cd2ffdd3402f20ab6b
RandieSasongko <113977205+RandieSasongko@users.noreply.github.com>
2023-06-04 22:46:04 +0700

    6/4/2023 10:45PM

--- app/src/main/java/com/example/project_akhir_uas_pemogramman_aplikasi_bergerak_ii/Activity/TambahActivity.java
+++ app/src/main/java/com/example/project_akhir_uas_pemogramman_aplikasi_bergerak_ii/Activity/TambahActivity.java
@@ -3,10 +3,16 @@ package com.example.project_akhir_uas_pemogramman_aplikasi_bergerak_ii.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.project_akhir_uas_pemogramman_aplikasi_bergerak_ii.R;
 
 public class TambahActivity extends AppCompatActivity {
+
+    private EditText etNama, etKategori, etDeskripsi, etType, etKapasitas;
+    private Button btnSimpan;
+    private String nama, kategori, deskripsi, type, kapasitas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
